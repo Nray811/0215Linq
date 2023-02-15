22dbd26e95f89254331635ac3b6cd9e4a86bc56e
Nray <ray08118686@gmail.com>
2023-02-15 13:43:12 +0800

    加入專案檔案。

---
+++ LinqSample003/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace LinqSample003
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var list = CreateList();          
+            // 這裡的 person1 是單個物件, 也就是 MyData person1
+            var person1 = list.FirstOrDefault((x) => x.Age < 30);
+            Console.WriteLine($"小於 37 歲的人第一個被找到的是 : {person1.Name}");
+            // 因為找不到, 就會跳出例外
+            var person2 = list.First((x) => x.Age < 30);
+            Console.WriteLine($"小於 30 歲的人第一個被找到的是 : {person2.Name}");
+            Console.ReadLine();
+        }
+
+        static List<Mydata> CreateList()
+        {
+            return new List<Mydata>()
+                {
+                new Mydata { Name = "Bill", Age = 47 },
+                new Mydata { Name = "John", Age = 37 },
+                new Mydata { Name = "Tom", Age = 48 },
+                new Mydata { Name = "David", Age = 36 },
+                new Mydata { Name = "Bill", Age = 35 },
+            };
+        }
+    }
+}
+
+
+
+
+
+
+
+
---
+++ LinqSamples001/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinqSamples001
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Mydata> list = CreateList();
+            //Query Expression
+            //具有IEnumerable可以foreach
+            IEnumerable<Mydata> people = 
+                from data in list  where data.Name == "Bill" select data;           
+            foreach (Mydata person in people)
+            {
+                Console.WriteLine($"{person.Name}是{person.Age}歲");
+            }
+
+            var people1 = list.Where((x) => x.Name == "BIll");
+            foreach (Mydata person in people)
+            {
+                Console.WriteLine($"{person.Name}是{person.Age}歲");
+            }
+
+        }
+
+        static List<Mydata> CreateList()
+        {
+            return new List<Mydata>()
+                {
+                new Mydata { Name = "Bill", Age = 47 },
+                new Mydata { Name = "John", Age = 37 },
+                new Mydata { Name = "Tom", Age = 48 },
+                new Mydata { Name = "David", Age = 36 },
+                new Mydata { Name = "Bill", Age = 35 },
+            };
+        }
+
+
+
+    }
+}
